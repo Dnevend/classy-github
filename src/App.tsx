406c8084d343c22b8bcd3c175deed483afabf6a7
Dnevend <Dnevend@gmail.com>
2024-04-24 16:44:40 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} alt="React logo" />
+        </a>
+      </div>
+      <h1>Github Profile</h1>
+    </>
+  );
+}
+
+export default App;
