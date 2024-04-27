377ace5c49552eb9a1e0d86ef1f832e6a75eb128
Dnevend <Dnevend@gmail.com>
2024-04-27 22:15:10 +0800

    feat: config update

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -9,3 +9,18 @@ export type CachePrefix =
     | 'config'
 
 export type CacheKey = CachePrefix | `${CachePrefix}${string}` | string
+
+export interface ClassyConfig {
+    theme: Theme,
+    profile: {
+        repositories: {
+            visible: boolean,
+            showCount: number
+        }
+    },
+    gists: {
+        prefix: string
+        split: string
+    },
+    links: { title: string, href: string }[]
+}
