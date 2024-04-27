a9e09f7180c2010662e44f061114de126cbcf51d
Dnevend <Dnevend@gmail.com>
2024-04-27 19:29:48 +0800

    styles: layout

--- src/layout/normal/index.tsx
+++ src/layout/normal/index.tsx
@@ -1,23 +1,36 @@
 import Logo from "@/components/logo";
+import { useClassyParams } from "@/lib/hooks";
 import { Suspense } from "react";
 import { Outlet } from "react-router";
+import { Link } from "react-router-dom";
+import { Loading } from "./loading";
 
 export function NormalLayout() {
-  return (
-    <div className="wrapper">
-      <div className="full-bleed sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-        <div className="flex justify-between items-center mx-auto h-14 px-8 max-w-screen-xl items-center">
-          <Logo />
-          name
-        </div>
-      </div>
+  const { user } = useClassyParams();
 
-      <Suspense fallback={<p className="text-xl">Loading ...</p>}>
-        <Outlet />
+  return (
+    <main
+      className="wrapper min-h-screen"
+      style={{ gridTemplateRows: "auto 1fr auto" }}
+    >
+      <header className="full-bleed sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <div className="flex justify-between items-center mx-auto h-14 px-8 max-w-screen-xl items-center">
+          <nav className="flex items-center gap-4 text-sm lg:gap-6">
+            <Logo />
+            <Link to={`/${user}/gists`}>Gists</Link>
+          </nav>
+          <h1>{user}</h1>
+        </div>
+      </header>
+
+      <Suspense fallback={<Loading />}>
+        <div>
+          <Outlet />
+        </div>
       </Suspense>
 
-      <div className="p-2">footer</div>
-    </div>
+      <footer className="p-2">footer</footer>
+    </main>
   );
 }
 
---
+++ src/layout/normal/loading.tsx
@@ -0,0 +1,3 @@
+export const Loading = () => {
+  return <div className="min-h-screen text-xl">Loading ...</div>;
+};
