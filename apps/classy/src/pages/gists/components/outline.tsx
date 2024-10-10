97d882999090e5d257c8358b532d187a62bedd60
Dnevend <Dnevend@gmail.com>
2024-10-10 09:38:32 +0800

    feat: cache outline mode

--- apps/classy/src/pages/gists/components/outline.tsx
+++ apps/classy/src/pages/gists/components/outline.tsx
@@ -4,6 +4,7 @@ import { gitApiFetch, requestUrl, usePageFetch } from "@classy/lib";
 import { useQueryClient } from "@tanstack/react-query";
 import { Gist } from "@classy/types";
 import { Link } from "react-router-dom";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const Outline = ({ user }: { user: string }) => {
   const queryClient = useQueryClient();
@@ -16,7 +17,7 @@ const Outline = ({ user }: { user: string }) => {
     [user, queryClient]
   );
 
-  const { datalist } = usePageFetch<Gist>({
+  const { datalist, fetching } = usePageFetch<Gist>({
     pageSize: 99,
     fetchFunc: fetchGists,
   });
@@ -39,6 +40,15 @@ const Outline = ({ user }: { user: string }) => {
 
   return (
     <div className="flex flex-col space-y-8 max-w-full lg:max-w-[80%] mx-auto">
+      {fetching && (
+        <div className="flex flex-col space-y-3 my-8">
+          <Skeleton className="h-6 w-24" />
+          <Skeleton className="h-4 w-full rounded-xl" />
+          <Skeleton className="h-4 w-full rounded-xl" />
+          <Skeleton className="h-4 w-full rounded-xl" />
+        </div>
+      )}
+
       {groupList.map((it) => (
         <div key={it.year}>
           <h2 className="text-2xl font-bold text-gray-500 mb-2">{it.year}</h2>
--- apps/classy/src/pages/gists/index.tsx
+++ apps/classy/src/pages/gists/index.tsx
@@ -3,7 +3,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Switch } from "@/components/ui/switch";
 import { ExternalLink } from "lucide-react";
 import { useParams, Link } from "react-router-dom";
-import { cn } from "@classy/lib";
+import { cn, storeGet, storeSet } from "@classy/lib";
 import { githubUrl } from "@classy/shared";
 import AllGists from "./components/allGists";
 import SpecifyGists from "./components/specifyGists";
@@ -14,7 +14,10 @@ import { useState } from "react";
 export function Gists() {
   const { user } = useParams() as { user: string };
 
-  const [showOutline, setShowOutline] = useState(true);
+  const [showOutline, setShowOutline] = useState(
+    storeGet("config_gist_mode", true)
+  );
+
   const { defaultType, gistTypes, getDefaultType, onChangeType } =
     useGistsType(user);
 
@@ -25,32 +28,39 @@ export function Gists() {
         defaultValue={getDefaultType()}
         onValueChange={(v) => onChangeType(v)}
       >
-        <div className="flex items-center justify-between">
+        <div className="h-10 flex items-center justify-between">
           <div className="flex items-center space-x-1">
             <Switch
               id="detail"
               checked={showOutline}
-              onCheckedChange={(v) => setShowOutline(v)}
+              onCheckedChange={(v) => {
+                setShowOutline(v);
+                storeSet("config_gist_mode", v);
+              }}
             />
-            <label htmlFor="detail" className="text-sm text-gray-600">
-              Outline
-            </label>
+            {showOutline && (
+              <label htmlFor="detail" className="text-sm text-gray-600">
+                Outline
+              </label>
+            )}
           </div>
 
-          <TabsList>
-            <TabsTrigger className="px-3 capitalize" value={defaultType.name}>
-              {defaultType.name}
-            </TabsTrigger>
-            {gistTypes.map((it) => (
-              <TabsTrigger
-                className="px-3 capitalize"
-                key={it.name}
-                value={it.name}
-              >
-                {it.name}
+          {!showOutline && (
+            <TabsList>
+              <TabsTrigger className="px-3 capitalize" value={defaultType.name}>
+                {defaultType.name}
               </TabsTrigger>
-            ))}
-          </TabsList>
+              {gistTypes.map((it) => (
+                <TabsTrigger
+                  className="px-3 capitalize"
+                  key={it.name}
+                  value={it.name}
+                >
+                  {it.name}
+                </TabsTrigger>
+              ))}
+            </TabsList>
+          )}
         </div>
 
         {showOutline && (
